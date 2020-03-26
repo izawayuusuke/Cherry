dfac77bb0136a39963674b8a3bf062c92e03383a
井澤優介 <izawayuusuke@MacBookPro.local>
2020-03-26 21:30:17 +0900

    文字列

---
+++ variation.rb
@@ -0,0 +1,21 @@
+a = 123.to_s
+
+b = [1, 2, 3].join
+
+puts a,b
+
+c = 'yeah' * 10
+puts c
+
+d = String.new('hello')
+puts d
+
+puts "\u3042\u3044\u3046"
+
+puts "\u5504\u310111\u00555"
+
+x = 3
+y = 5
+puts x + y
+
+puts ?a
