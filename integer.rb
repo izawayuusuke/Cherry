aed3a5422380ee1e32adaca22f1acc67b7202b6e
井澤優介 <izawayuusuke@MacBookPro.local>
2020-03-26 21:42:34 +0900

    Numeric

--- integer.rb
+++ integer.rb
@@ -13,3 +13,15 @@ puts a.to_s(2)
 puts a.to_s(8)
 puts a.to_s(16)
 puts a.to_s(10)
+
+puts 2e3
+
+puts 10.class
+puts 9999999.class
+
+r =2/3r
+r = '2/3'.to_r
+puts r.class.superclass
+
+c = '0.3-0.5'.to_c
+puts c.class.superclass
